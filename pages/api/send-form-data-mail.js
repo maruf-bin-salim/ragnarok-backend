63c339a4a22cebc8affc67de80ea27819f677c8a
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-07-01 03:01:22 +0600

    update

---
+++ pages/api/send-form-data-mail.js
@@ -0,0 +1,76 @@
+const nodemailer = require('nodemailer');
+
+
+
+
+
+export default function handler(req, res) {
+
+    // Only allow POST requests
+    if (req.method !== 'POST') {
+        res.status(404).json({
+            error: "Method not allowed"
+        });
+        return;
+    }
+
+
+    if(!req.body.email)
+    {
+        res.status(404).json({
+            error: "Invalid request"
+        });
+        return;
+    }
+
+
+
+
+    let html = `
+        <div style="border: 3px solid #000; padding: 10px;">
+            <p> Name : ${req.body.name} </p> <br />
+            <p> Email Address : ${req.body.email} </p> <br />
+            <p> Phone Number : ${req.body.phone} </p> <br />
+            <p> User's Message : ${req.body.message} </p>
+        </div>
+    `;
+
+  
+
+    // create message object
+    const message = {
+        from: "creditriver01@gmail.com", // Sender address
+        to: "marufbinsalim01@gmail.com", // List of recipients
+        subject: "New Message from a user on The Credit River Company Website, Contact Form",
+        text: "",
+        html: html,
+    };
+
+    // create transporter object
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "creditriver01@gmail.com",
+            pass: "tjtnsaiifbqzaybf",
+        },
+    });
+
+    // send mail
+    transporter.sendMail(message, (err, info) => {
+
+        // if error, return error
+        if (err) {
+            res.status(404).json({
+                error: `${err}`
+            });
+        } else {
+            // if success, return success message
+            res.status(250).json({
+                success: `Message delivered to : ${info.accepted}`
+            });
+        }
+    });
+
+}
+
+// curl -X POST -H "Content-Type: application/json" -d '{"name":"Maruf Bin Salim","email":"marufbinsalim01@gmail","phone":"647-786-1234","message":"Hello, I am interested in your products"}' http://localhost:3000/api/send-form-data-mail
---
+++ pages/api/send-subscribe-data-mail.js
@@ -0,0 +1,64 @@
+const nodemailer = require('nodemailer');
+
+export default function handler(req, res) {
+    // Only allow POST requests
+    if (req.method !== 'POST') {
+        res.status(404).json({
+            error: 'Method not allowed'
+        });
+        return;
+    }
+
+    // get email from request body
+    const email = req.body.email;
+
+    if(!email) {
+        res.status(404).json({
+            error: 'Invalid request'
+        });
+        return;
+    }
+
+    // create html template
+    const html = `
+        <div style="border: 3px solid #000; padding: 10px;">
+            A user with Email "${email}" has signed up for info on new products and discount code
+        </div>
+    `;
+
+    // create message object
+    const message = {
+        from: 'creditriver01@gmail.com', // Sender address
+        to: 'marufbinsalim01@gmail.com', // List of recipients
+        subject: 'New Sign-up for Product Info and Discount Code, on The Credit River Company Website',
+        text: '',
+        html: html
+    };
+
+    // create transporter object
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'creditriver01@gmail.com',
+            pass: 'tjtnsaiifbqzaybf'
+        }
+    });
+
+    // send mail
+    transporter.sendMail(message, (err, info) => {
+        // if error, return error
+        if (err) {
+            res.status(500).json({
+                error: `${err}`
+            });
+        } else {
+            // if success, return success message
+            res.status(200).json({
+                success: `Message delivered to: ${info.accepted}`
+            });
+        }
+    });
+}
+
+
+// curl -X POST -H "Content-Type: application/json" -d '{"email":"marufbinsalim01@gmail"}' http://localhost:3000/api/send-subscribe-data-mail
